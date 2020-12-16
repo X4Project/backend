35a30bb96c71a3f7efebbf9dde71fe2eb2060b31
Shawn <nhatquangta2911@gmail.com>
2020-12-16 23:08:52 +0700

    Fix token in header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies['token'] || req.headers['x-auth-token'];
+  const token =
+    req.cookies['token'] || req.headers['x-auth-token'] || req.headers['token'];
   if (!token) return res.status(401).send('Access denied. No token provided.');
   try {
     const decodedPayload = jwt.verify(token, config.JWT_SECRET_KEY);
