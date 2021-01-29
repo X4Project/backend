e578ab79fed4a8d1105cd860a669062d32837723
Shawn <nhatquangta2911@gmail.com>
2021-01-29 09:04:21 +0700

    Bait commit

--- startup/cron-jobs.js
+++ startup/cron-jobs.js
@@ -2,26 +2,26 @@ const cron = require('node-cron');
 const axios = require('axios');
 const { logger } = require('../middlewares/logging');
 
-cron.schedule('00 */5 * * * *', async () => {
-  try {
-    await axios.get('https://x4-disease-api.herokuapp.com');
-  } catch (error) {
-    logger.error(error.message, error);
-  }
-});
+// cron.schedule('00 */5 * * * *', async () => {
+//   try {
+//     await axios.get('https://x4-disease-api.herokuapp.com');
+//   } catch (error) {
+//     logger.error(error.message, error);
+//   }
+// });
 
-cron.schedule('00 */5 * * * *', async () => {
-  try {
-    await axios.get('https://x4-diseases.herokuapp.com');
-  } catch (error) {
-    logger.error(error.message, error);
-  }
-});
+// cron.schedule('00 */5 * * * *', async () => {
+//   try {
+//     await axios.get('https://x4-diseases.herokuapp.com');
+//   } catch (error) {
+//     logger.error(error.message, error);
+//   }
+// });
 
-cron.schedule('00 */5 * * * *', async () => {
-  try {
-    await axios.get('https://x4-admin.herokuapp.com');
-  } catch (error) {
-    logger.error(error.message, error);
-  }
-});
+// cron.schedule('00 */5 * * * *', async () => {
+//   try {
+//     await axios.get('https://x4-admin.herokuapp.com');
+//   } catch (error) {
+//     logger.error(error.message, error);
+//   }
+// });
